d6075b0ae3434b99bebe9ef66406011fa565af44
Garo028 <maposacourage41@gmail.com>
2025-12-18 22:33:57 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Scotty-C',
+  author: 'Scotty-C',
+  botName: "Scotty-C",
+  botOwner: 'Scotty', // Your name
+  ownerNumber: '263788114185', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'NrSjG6var2uiuSYDm0xTqCX0xcFgGj4s',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+  updateZipUrl: "https://github.com/Garo028/Scotty-C/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
